110db86462aaafc927bf5be979fc785da7c274be
Mitchell Klijs <mitchell@mk-software-solutions.nl>
2026-05-31 16:34:38 +0200

    chore: initial version

--- teksttv.php
+++ teksttv.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: TekstTV
  * Description: Beheer tekst-tv slides en lever ze via een REST API voor de TekstTV frontend.
- * Version: 1.0.0
+ * Version: 0.0.1
  * Author: ZuidWest
  * Author URI: https://www.zuidwesttv.nl/
  * Plugin URI: https://github.com/oszuidwest/teksttv-wp-plugin
@@ -16,7 +16,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('TEKSTTV_VERSION', '1.0.0');
+define('TEKSTTV_VERSION', '0.0.1');
 define('TEKSTTV_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('TEKSTTV_PLUGIN_URL', plugin_dir_url(__FILE__));
 
